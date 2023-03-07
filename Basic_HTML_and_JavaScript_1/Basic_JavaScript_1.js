595fe21d76be2c6acbdbebab46a72b030facf3a5
Pablo <psalazar5@live.com>
2023-03-07 12:54:36 -0500

    Html & Css projects

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+<scipt>
+
+	function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		doucment.getElementByID("Irish").innerHTML =
+		result ;
+	}
+</script
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HTML-and-CSS-Projects
+
 This repository will contain my following work displayed throughout my training in the Tech Academy bootcamp.
